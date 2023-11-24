ded207b48d5729a4db472c03525504e77eed4714
John Richard Palmos <99818180+chadkunsenpai@users.noreply.github.com>
2023-11-24 14:18:43 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
-import 'package:cc206_kaon_/features/sign_up_page.dart';
+import 'package:cc206_kaon_/features/Log-in_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme:
               ColorScheme.fromSeed(seedColor: Color.fromRGBO(145, 199, 137, 1)),
           useMaterial3: true,
         ),
-        home: const SignupPage());
+        home: const LoginPage());
   }
 }
